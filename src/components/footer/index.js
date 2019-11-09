2830e725399726d59e1373729965f2ca65003dd8
Ryan Rafilianto <ryan.rafilianto@amarbank.co.id>
2019-11-09 17:37:45 +0700

    inital commit

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      {/* <div>Copyright</div> */}
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/header/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const Wrapper = styled.header`
+  background: #fff;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 8px 0px;
+`;
+
+const Title = styled.h1`
+  padding: 20px;
+  margin: 0px;
+  text-align: center
+`;
+
+const Header = () => {
+  return (
+    <Wrapper>
+      <Title>Welcome to Todo</Title>
+    </Wrapper>
+  )
+}
+
+export default Header;
